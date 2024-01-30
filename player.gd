beae68ee048de0062e42886768308df37a0907dc
WinterAtne <Winter54b@gmail.com>
2024-01-29 23:47:29 -0700

    Made the player move
    At somepoint i thought godot physics was the problem so i added a new physics engine lol < 3

---
+++ player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var acceleration = 4.0
+@export var jump_speed = 8.0
+@export var mouse_sensitivity = 0.0015
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera = $Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	get_move_input(delta)
+	move_and_slide()
+	
+	pass
+
+func get_move_input(delta):
+	#We store the state of the y velocity so that it isn't canceled when we change everything else
+	var vy = velocity.y
+	
+	velocity.y = 0
+	var input = Input.get_vector("left", "right", "forward", "backward")
+	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, rotation.y)
+	velocity = lerp(velocity, dir * speed, acceleration * delta)
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y = jump_speed
+	else:
+		velocity.y = vy
+	
+	pass
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		camera.rotation.x -= event.relative.y * mouse_sensitivity
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90.0, 90.0)
+		rotation.y -= event.relative.x * mouse_sensitivity
